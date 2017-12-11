f2a21578a620063498349f209fecccb7b0c13954
Adro.Codes <hurricane.interactive@gmail.com>
2017-12-11 22:04:56 +1100

    Use app environment settings / import db models

--- server/server.py
+++ server/server.py
@@ -1,16 +1,19 @@
+import os
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask.ext.sqlalchemy import SQLAlchemy
 from flask_restful import Api
 
 from endpoints.quotes import Quotes
 
 
 app = Flask(__name__, template_folder="../public", static_folder="../public/dist")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/quick_quotes'
-api = Api(app)
-
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+from models import Result
+
+api = Api(app)
 api.add_resource(Quotes, '/api/quotes')
 
 @app.route("/", defaults={'path': ''})
