708f555e4660ebf40249020c76b0ff46a18b6be9
Adro.Codes <hurricane.interactive@gmail.com>
2017-12-13 21:57:41 +1100

    Import Quote Model

--- server/server.py
+++ server/server.py
@@ -1,9 +1,7 @@
 import os
 from flask import Flask, render_template
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
-
-from endpoints.quotes import Quotes
 
 
 app = Flask(__name__, template_folder="../public", static_folder="../public/dist")
@@ -11,7 +9,11 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import Result
+# Database Models
+from Models import *
+
+
+from endpoints.quotes import Quotes
 
 api = Api(app)
 api.add_resource(Quotes, '/api/quotes')
